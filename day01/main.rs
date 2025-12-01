8f1fe695b0db7b919ac4d9cac1a865c895c2dbf0
Zach Thompson <zach@allotropic.com>
2025-12-01 16:46:53 -0500

    Solve day 1

--- day01/main.rs
+++ day01/main.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("Part 1:");
+    let input = include_str!("input.txt");
+    let (part1, part2) = run(input);
+    println!("Part 1: {}", part1);
+    println!("Part 1: {}", part2);
+}
+
+fn run(input: &str) -> (u32, u32) {
+    let mut pos = 50;
+    let mut zeroes_landed = 0;
+    let mut zeroes_clicked = 0;
+
+    for (dir, val) in input.lines().map(|line| {
+        let (dir, val) = line.split_at(1);
+        let val = val.parse::<u32>().unwrap();
+        (dir, val)
+    }) {
+        match dir {
+            "R" => {
+                let tot = pos + val;
+                pos = tot % 100;
+                zeroes_clicked += tot / 100;
+            }
+            "L" => {
+                let tot = ((100 - pos) % 100) + val;
+                pos = (100 - (tot % 100)) % 100;
+                zeroes_clicked += tot / 100;
+            }
+            _ => unreachable!(),
+        }
+
+        if pos == 0 {
+            zeroes_landed += 1;
+        }
+    }
+
+    (zeroes_landed, zeroes_clicked)
 }
