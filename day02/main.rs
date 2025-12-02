665f194d4c4c57b8ea125771171982f0d85dbb75
Zach Thompson <zach@allotropic.com>
2025-12-02 09:16:19 -0500

    Clean up

--- day02/main.rs
+++ day02/main.rs
@@ -8,7 +8,6 @@ fn main() {
         .map(|x| x.split_once('-').unwrap())
         .map(|(a, b)| a.parse::<usize>().unwrap()..=b.parse::<usize>().unwrap())
         .collect();
-
     println!("Part 1: {}", part1(ranges.clone()));
     println!("Part 2: {}", part2(ranges));
 }
@@ -32,24 +31,19 @@ fn part1(ranges: Vec<RangeInclusive<usize>>) -> usize {
 fn part2(ranges: Vec<RangeInclusive<usize>>) -> usize {
     let mut invalid: HashSet<usize> = HashSet::new();
     for r in ranges {
-        //println!("========== {r:?} ===========");
         for i in r {
-            //println!("---- {i} ----");
             let i_str = i.to_string();
             if i_str.len() < 2 {
                 continue;
             }
             'each_len: for len in 1..=i_str.len() / 2 {
-                //println!("len: {len}");
                 if i_str.len().is_multiple_of(len) {
                     let first = &i_str[0..len];
-                    //println!(" .. is multiple len: {len}, first: {first}");
                     for j in 1..i_str.len() / len {
                         if first != &i_str[j * len..j * len + len] {
                             continue 'each_len;
                         }
                     }
-                    //println!("!!!!!!!!!!!!!!!!!!!!! got one: {i}");
                     invalid.insert(i);
                 }
             }
