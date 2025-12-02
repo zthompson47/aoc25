0d369be9dc47a73e39ac6de5856056ce0bdc4930
Zach Thompson <zach@allotropic.com>
2025-12-02 06:49:22 -0500

    Clean up and document

--- day01/main.rs
+++ day01/main.rs
@@ -2,34 +2,34 @@ fn main() {
     let input = include_str!("input.txt");
     let (part1, part2) = run(input);
     println!("Part 1: {}", part1);
-    println!("Part 1: {}", part2);
+    println!("Part 2: {}", part2);
 }
 
 fn run(input: &str) -> (u32, u32) {
-    let mut pos = 50;
+    let mut position = 50;
     let mut zeroes_landed = 0;
     let mut zeroes_clicked = 0;
 
-    for (dir, val) in input.lines().map(|line| {
-        let (dir, val) = line.split_at(1);
-        let val = val.parse::<u32>().unwrap();
-        (dir, val)
+    for (direction, delta) in input.lines().map(|line| {
+        let (direction, delta) = line.split_at(1);
+        let delta = delta.parse::<u32>().unwrap();
+        (direction, delta)
     }) {
-        match dir {
+        match direction {
             "R" => {
-                let tot = pos + val;
-                pos = tot % 100;
-                zeroes_clicked += tot / 100;
+                let distance = position + delta;
+                position = distance % 100;
+                zeroes_clicked += distance / 100;
             }
             "L" => {
-                let tot = ((100 - pos) % 100) + val;
-                pos = (100 - (tot % 100)) % 100;
-                zeroes_clicked += tot / 100;
+                let distance = ((100 - position) % 100) + delta;
+                position = (100 - (distance % 100)) % 100;
+                zeroes_clicked += distance / 100;
             }
             _ => unreachable!(),
         }
 
-        if pos == 0 {
+        if position == 0 {
             zeroes_landed += 1;
         }
     }
