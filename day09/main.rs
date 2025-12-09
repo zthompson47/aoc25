e3adf1e9440e3495b349efff365e68f496bf8e91
Zach Thompson <zach@allotropic.com>
2025-12-09 09:22:24 -0500

    Implement  trait

--- day09/main.rs
+++ day09/main.rs
@@ -9,22 +9,30 @@ fn main() {
         })
         .collect();
     let mut max_area = 0;
-    for (a, b) in pairs(&tiles) {
+    for (a, b) in tiles.pairs() {
         let area = ((a.0 - b.0 + 1) * (a.1 - b.1 + 1)).abs();
         max_area = max_area.max(area);
     }
     println!("Part 1: {max_area}");
 }
 
-fn pairs<T>(v: &[T]) -> Vec<(T, T)>
-where
-    T: Clone + Copy,
-{
-    (0..v.len() - 1)
-        .flat_map(|x| {
-            (x + 1..v.len())
-                .map(move |y| (v[x], v[y]))
-                .collect::<Vec<_>>()
-        })
-        .collect()
+trait Pairs<T> {
+    fn pairs(&self) -> Vec<(T, T)>
+    where
+        T: Clone + Copy;
+}
+
+impl<T> Pairs<T> for Vec<T> {
+    fn pairs(&self) -> Vec<(T, T)>
+    where
+        T: Clone + Copy,
+    {
+        (0..self.len() - 1)
+            .flat_map(|x| {
+                (x + 1..self.len())
+                    .map(move |y| (self[x], self[y]))
+                    .collect::<Vec<_>>()
+            })
+            .collect()
+    }
 }
